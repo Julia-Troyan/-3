c70f3aed219865070a8249b10b0da2f74cb215aa
Julia-Tryan <uliana.troyan@mail.ru>
2023-10-23 15:16:28 +0300

    работа с массивом

--- Exsample3/Program.cs
+++ Exsample3/Program.cs
@@ -87,28 +87,66 @@
 
 // Ясна ли задача?
 
-string text = "- Я думаю, - сказал князь, улыбаясь, - что, "
-           + "ежели бы вас поставили вместо нашего милого Винценгероде,"
-           + "вы бы взяли приступом согласие прусского короля."
-           + "Вы так красноречивы. Вы дадите мне чаю?";
+// string text = "- Я думаю, - сказал князь, улыбаясь, - что, "
+//            + "ежели бы вас поставили вместо нашего милого Винценгероде,"
+//            + "вы бы взяли приступом согласие прусского короля."
+//            + "Вы так красноречивы. Вы дадите мне чаю?";
 //string s = "qwerty"
 //          012345
 //s [3] //r
-string Replace(string text, char oldValue, char newValue)
+// string Replace(string text, char oldValue, char newValue)
+// {
+//     string result = String.Empty;
+//     int Length = text.Length;
+//     for (int i = 0; i < Length; i++)
+//     {
+//         if (text[i] == oldValue) result = result + $"{newValue}";
+//         else result = result + $"{text[i]}";
+//     }
+
+//     return result;
+// }
+// string newText = Replace(text, ' ', '|');
+// System.Console.WriteLine(newText);
+
+// System.Console.WriteLine();
+// newText = Replace(newText, 'к', 'К');
+// System.Console.WriteLine(newText);
+
+
+// 1. Найти позицию минимального элемента 
+// в не отсортированной части массива.
+// 2. Произвести обмен этого значения со значением первой неотсортированной позиции.
+// 3. Повторять пока есть не отсортированный элементы.
+
+int[] arr = { 1, 5, 4, 3, 2, 6, 7, 1, 1 };
+void PrintArray(int[] array)
 {
-    string result = String.Empty;
-    int Length = text.Length;
-    for (int i = 0; i < Length; i++)
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
     {
-        if (text[i] == oldValue) result = result + $"{newValue}";
-        else result = result + $"{text[i]}";
+        System.Console.Write($"{array[i]} ");
     }
-
-    return result;
+    System.Console.WriteLine();
 }
-string newText = Replace(text, ' ', '|');
-System.Console.WriteLine(newText);
 
-System.Console.WriteLine();
-newText = Replace(newText, 'к', 'К');
-System.Console.WriteLine(newText);
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] < array[minPosition]) minPosition = j;
+        }
+
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+    }
+}
+PrintArray(arr);
+SelectionSort(arr);
+
+PrintArray(arr);

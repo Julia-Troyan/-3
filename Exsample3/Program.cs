dc8a0d5b7035a70cebcc5f52d4bfe070112bca1a
Julia-Tryan <uliana.troyan@mail.ru>
2023-10-23 14:38:17 +0300

    работа с текстом

--- Exsample3/Program.cs
+++ Exsample3/Program.cs
@@ -1,37 +1,37 @@
 ﻿// Вид 1
-void Method1()
-{
-    Console.WriteLine("Автор Троян Юлия");
-}
+// void Method1()
+// {
+//     Console.WriteLine("Автор Троян Юлия");
+// }
 // Method1();
 
 // Вид 2
-void Method2(string msg)
-{
-    Console.WriteLine(msg);
-}
-// Method2(msg: "Текст сообщения");
+// void Method2(string msg)
+// {
+//     Console.WriteLine(msg);
+// }
+// // Method2(msg: "Текст сообщения");
 
-void Method21(string msg, int count)
-{
-    int i = 0;
-    while (i < count)
-    {
-        Console.WriteLine(msg);
-        i++;
-    }
-}
+// void Method21(string msg, int count)
+// {
+//     int i = 0;
+//     while (i < count)
+//     {
+//         Console.WriteLine(msg);
+//         i++;
+//     }
+// }
 // Method21(msg: "Текст", count: 4);
 // Method21(count: 4, msg: "новый текст");
 
 // Вид 3
 
-int Method3()
-{
-    return DateTime.Now.Year;
-}
+// int Method3()
+// {
+//     return DateTime.Now.Year;
+// }
 
-int year = Method3();
+// int year = Method3();
 // Console.WriteLine(year);
 
 // Вид 4
@@ -65,26 +65,46 @@ int year = Method3();
 // string res = Method4(10, "z");
 // Console.WriteLine(res);
 
-string Method4(int count, string text)
-{
-    string result = String.Empty;
-    for (int i = 0; i < count; i++)
-    {
-        result = result + text;
-    }
-    return result;
-}
-string res = Method4(10, "z");
-Console.WriteLine(res);
+// string Method4(int count, string text)
+// {
+//     string result = String.Empty;
+//     for (int i = 0; i < count; i++)
+//     {
+//         result = result + text;
+//     }
+//     return result;
+// }
+// string res = Method4(10, "z");
+// Console.WriteLine(res);
 
 // Создаем цикл for внутри которого находится другой цикл for
 // Напишем программу, которая выводит таблицу умнажения
 
-for (int i = 2; i <= 10; i++)
+// Задача. Работа с текстом.
+// Дан текст. В тексте нужно все пробелы записать черточками,
+// маленькие буквы "к" заменить большими "К", а большие "С" 
+// заменить маленькими "с".
+
+// Ясна ли задача?
+
+string text = "- Я думаю, - сказал князь, улыбаясь, - что, "
+           + "ежели бы вас поставили вместо нашего милого Винценгероде,"
+           + "вы бы взяли приступом согласие прусского короля."
+           + "Вы так красноречивы. Вы дадите мне чаю?";
+//string s = "qwerty"
+//          012345
+//s [3] //r
+string Replace(string text, char oldValue, char newValue)
 {
-    for (int j = 2; j <= 10; j++)
+    string result = String.Empty;
+    int Length = text.Length;
+    for (int i = 0; i < Length; i++)
     {
-        System.Console.WriteLine($"{i}*{j} = {i * j}");
+        if (text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
     }
-    System.Console.WriteLine();
+
+    return result;
 }
+string newText = Replace(text, ' ', '|');
+System.Console.WriteLine(newText);
